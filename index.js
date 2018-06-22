b8d8bebf36d251baaf64a95b9ab95b0e966a74d6
Guy Depay <guy.depay@kdi.kongsberg.com>
2018-06-22 08:53:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const handler = (req, res) => {
+    console.log("Server received request");
+    res.end("Hello world");
+};
+
+const server = http.createServer(handler);
+
+server.listen(port, () => {
+    console.log(`Listening on port:${port}`);
+});
