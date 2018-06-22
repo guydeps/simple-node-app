c8a5a67bb30e37230507aff734005a358da72a95
Guy Depay <guy.depay@kdi.kongsberg.com>
2018-06-22 12:19:14 +0200

    changed message to azure git

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log("Server received request");
-    res.end("Hello from Git hub !!!");
+    res.end("Hello from Azure git!!!");
 };
 
 const server = http.createServer(handler);
