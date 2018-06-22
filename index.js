93235220a0a3b1b673d6df129218e465cb3fa3d8
Guy Depay <guy.depay@kdi.kongsberg.com>
2018-06-22 09:39:59 +0200

    changed message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log("Server received request");
-    res.end("Hello from Azure git");
+    res.end("Hello from Azure git again!");
 };
 
 const server = http.createServer(handler);
